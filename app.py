42b1118a6db885974bfe9c944bbe41e5ac4a2449
kreindo <ahmadsan4p@gmail.com>
2023-04-04 18:40:01 +0800

    my ai

---
+++ app.py
@@ -0,0 +1,73 @@
+import os
+import openai
+from utils.prompter import *
+
+os.environ['API_KEY'] = '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'
+
+openai.organization = 'org-8afkp4JUkwZksD0mZLVHAQwO'
+openai.api_key = os.getenv('API_KEY')
+
+conversation = []
+history = {"history": conversation}
+mode = 0
+total_char = 0
+
+
+def defPrompt():
+    result = input("Enter prompt: ")
+    conversation.append({'role': 'user', 'content': result})
+    openai_answer()
+
+
+def openai_answer():
+    global total_char, conversation
+
+    for item in conversation:
+        if isinstance(item, dict) and "content" in item:
+            content = item["content"]
+            total_char += len(content)
+
+    while total_char > 4000:
+        try:
+            conversation.pop(2)
+            total_char -= len(conversation[2]["content"])
+        except:
+            print("Error: Prompt too long!")
+
+    with open("conversation.json", "w", encoding="utf-8") as f:
+        # Write the message data to the file in JSON format
+        json.dump(history, f, indent=4)
+
+    prompt = getPrompt()
+
+    response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=prompt,
+        max_tokens=128,
+        temperature=1,
+        top_p=0.9
+    )
+    message = response['choices'][0]['message']['content']
+    print(message)
+    conversation.append({'role': 'assistant', 'content': message})
+
+
+if __name__ == "__main__":
+    try:
+        # You can change the mode to 1 if you want to record audio from your microphone
+        # or change the mode to 2 if you want to capture livechat from youtube
+        mode = input("Mode (1-Text, 2-Youtube Live): ")
+
+        if mode == "1":
+            while True:
+                defPrompt()
+
+        elif mode == "2":
+            print("currently unimplemented")
+            # live_id = input("Livestream ID: ")
+            # # Threading is used to capture livechat and answer the chat at the same time
+            # t = threading.Thread(target=preparation)
+            # t.start()
+            # get_livechat(live_id)
+    except KeyboardInterrupt:
+        print("Stopped")
---
+++ utils/prompter.py
@@ -0,0 +1,62 @@
+import json
+import sys
+
+sys.stdout = open(sys.stdout.fileno(), mode='w', encoding='utf8', buffering=1)
+
+outputNum = 20
+
+
+def getIdentity(identityPath):
+    with open(identityPath, "r", encoding="utf-8") as f:
+        identityContext = f.read()
+    return {"role": "user", "content": identityContext}
+
+
+def getPrompt():
+    total_len = 0
+    prompt = []
+    prompt.append(getIdentity("charConfig/personality.txt"))
+    prompt.append(
+        {"role": "system", "content": f"Below is conversation history.\n"})
+
+    with open("conversation.json", "r") as f:
+        data = json.load(f)
+
+    history = data["history"]
+
+    for message in history[:-1]:
+        prompt.append(message)
+
+    prompt.append(
+        {
+            "role": "system",
+            "content": f"Here is the latest conversation.\n*Make sure your response is within {outputNum} characters!\n",
+        }
+    )
+
+    prompt.append(history[-1])
+
+    for item in prompt:
+        if isinstance(item, dict) and "content" in item:
+            content = item["content"]
+            total_len += len(content)
+
+    while total_len > 4000:
+        try:
+            # print(total_len)
+            # print(len(prompt))
+            prompt.pop(2)
+            total_len -= len(prompt[2]["content"])
+        except:
+            print("Error: Prompt too long!")
+
+    # total_characters = sum(len(d['content']) for d in prompt)
+    # print(f"Total characters: {total_characters}")
+
+    return prompt
+
+
+if __name__ == "__main__":
+    prompt = getPrompt()
+    print(prompt)
+    print(len(prompt))
